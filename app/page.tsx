d0951345a89744a1982a093678194305cf5226d4
Fabian Gander <fabian.gander@soulcode.ch>
2024-10-01 14:17:37 +0200

    feat(page) Adding a v0 starter layout for the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,74 +1,9 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import ProductLandingPage from "./ProductLandingPage";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-
-        <div className="p-4 w-full flex justify-center" style={{ backgroundColor: "rgb(50 187 165)" }}>
-          <Image
-            className=""
-            src="/momo-transparent-icon.png"
-            alt="Momo logo"
-            width={40}
-            height={40}
-            priority
-          />
-        </div>
-        <h1 className="text-4xl font-bold text-center sm:text-left">
-          Welcome to Momo
-        </h1>
-
-        <Button>Test button</Button>
-      </main>
-      {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file-text.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer> */}
+    <div className="font-sans">
+      <ProductLandingPage />
     </div>
   );
 }
