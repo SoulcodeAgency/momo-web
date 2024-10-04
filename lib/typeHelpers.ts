5643e90ad0d980a94e909e078c98075444f3c157
Fabian Gander <fabian.gander@soulcode.ch>
2024-10-04 17:18:11 +0200

    feat(TS) Extending CanvasProps helper with parameters (canvas fields)

--- lib/typeHelpers.ts
+++ lib/typeHelpers.ts
@@ -3,9 +3,10 @@ import { ComponentProps } from "@uniformdev/canvas-next-rsc/component";
 /**
  * A type alias (generic type) that represents the properties of a Canvas component.
  *
- * @template TProps - The type of the properties for the Canvas component.
+ * @template TProps - The props of the UI component for which this canvas component is created.
+ * @template TParameters - The parameters of the Canvas component.
  * @template TSlots - A string literal type representing the slots available in the Canvas component.
  * 
  * @returns The properties of a Canvas component without the children property.
  */
-export type CanvasProps<TProps, TSlots extends string> = ComponentProps<Omit<TProps, 'children'>, TSlots>;
+export type CanvasProps<TProps, TParameters, TSlots extends string> = ComponentProps<Omit<TProps, 'children'> & TParameters, TSlots>;
