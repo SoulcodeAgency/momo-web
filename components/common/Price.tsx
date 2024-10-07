fd6b2f6411a5eb35198937432f5495597f8569e1
Fabian Gander <fabian.gander@soulcode.ch>
2024-10-07 11:24:26 +0200

    feat(Price) Adding price component

---
+++ components/common/Price.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react';
+
+export type PriceProps = {
+  price: string | ReactNode;
+  description?: string | ReactNode;
+  currency?: string | ReactNode;
+}
+
+export default function Card({ price, description, currency }: PriceProps) {
+  return (
+    <div className='text-center'>
+      <p className="text-4xl font-bold">{price} {currency}</p>
+      {description && (<p className="text-sm text-gray-500">{description}</p>)}
+    </div>
+  )
+}
+
---
+++ integrations/uniform/canvas/CanvasPrice.tsx
@@ -0,0 +1,17 @@
+import { UniformSlot, UniformText } from "@uniformdev/canvas-next-rsc/component";
+import { CanvasProps } from "@/lib/typeHelpers";
+import Price, { type PriceProps } from "@/components/common/Price";
+
+type Parameters = {};
+type SlotNames = string;
+type Props = CanvasProps<Parameters, SlotNames, PriceProps>;
+
+export function CanvasPrice({ context, component }: Props) {
+  // Provide the uniform fields for inline editing, instead of passing strings directly
+  const uniformPrice = <UniformText context={context} component={component} parameterId="price" placeholder="0" />;
+  const uniformCurrency = <UniformText context={context} component={component} parameterId="currency" placeholder="$" />;
+  const uniformDescription = <UniformText context={context} component={component} parameterId="description" placeholder="per user / month" />;
+  return (
+    <Price price={uniformPrice} description={uniformDescription} currency={uniformCurrency} />
+  );
+}
