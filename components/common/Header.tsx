d6d45d87cb2191fd2aac819956f989732aaea84a
Fabian Gander <fabian.gander@soulcode.ch>
2024-10-08 12:13:53 +0200

    feat(Header) Improving header so the layout knows its size, to not cover things behind

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -9,24 +9,27 @@ interface HeaderProps {
 
 export default function Header({ children }: HeaderProps) {
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 bg-primary border-b shadow-sm">
-      <div className="container mx-auto px-4 lg:px-6 h-16 flex items-center justify-between">
-        <Link className="flex items-center justify-center" href="#">
-          <Image className="" src="/momo-transparent-icon.png" alt="Momo logo" width={40} height={40} priority />
-          <span className="ml-2 text-lg font-bold">Momo</span>
-        </Link>
-        <nav className="flex gap-4 sm:gap-6">
-          <LinkList
-            links={[
-              { href: '#features', text: 'Features' },
-              { href: '#pricing', text: 'Pricing' },
-              { href: '#contact', text: 'Contact' },
-            ]}
-          />
+    <header className="bg-primary h-16 w-full">
+      <div className="fixed top-0 left-0 right-0 z-50 bg-primary border-b shadow-sm">
+        <div className="container h-16 mx-auto px-4 lg:px-6 flex items-center justify-between">
+          <Link className="flex h-16 items-center justify-center" href="#">
+            <Image className="" src="/momo-transparent-icon.png" alt="Momo logo" width={40} height={40} priority />
+            <span className="ml-2 text-lg font-bold">Momo</span>
+          </Link>
+          <nav className="flex gap-4 sm:gap-6">
+            <LinkList
+              links={[
+                { href: '#features', text: 'Features' },
+                { href: '#pricing', text: 'Pricing' },
+                { href: '#contact', text: 'Contact' },
+              ]}
+            />
 
-          {children}
-        </nav>
+            {children}
+          </nav>
+        </div>
       </div>
+
     </header>
   );
 }
