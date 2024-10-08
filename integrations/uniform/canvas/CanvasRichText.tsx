aaa4ad67abab8ee0a89c982481e826c44395b670
Fabian Gander <fabian.gander@soulcode.ch>
2024-10-08 12:14:20 +0200

    feat(RichText) Implemented CanvasRichText to enable Richtext editing

---
+++ integrations/uniform/canvas/CanvasRichText.tsx
@@ -0,0 +1,14 @@
+import { CanvasProps } from '../uniformTypes';
+import { UniformRichText } from '@uniformdev/canvas-next-rsc/component';
+
+type Parameters = { text: string };
+type SlotNames = string;
+type Props = CanvasProps<Parameters, SlotNames>;
+
+export function CanvasRichText({ component, context }: Props) {
+  return (
+    <div className='text-left'>
+      <UniformRichText context={context} component={component} parameterId="text" placeholder={'Enter text here'} />
+    </div>
+  );
+}
