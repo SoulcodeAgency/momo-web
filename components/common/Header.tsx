194d19d6185a2748c821427930c56cafdffa4dab
Fabian Gander <fabian.gander@soulcode.ch>
2024-10-08 21:20:21 +0200

    feat(Header) Momo logo links to the root of the landing page

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -12,7 +12,7 @@ export default function Header({ children }: HeaderProps) {
     <header className="bg-primary h-16 w-full">
       <div className="fixed top-0 left-0 right-0 z-50 bg-primary border-b shadow-sm">
         <div className="container h-16 mx-auto px-4 lg:px-6 flex items-center justify-between">
-          <Link className="flex h-16 items-center justify-center" href="#">
+          <Link className="flex h-16 items-center justify-center" href="/">
             <Image className="" src="/momo-transparent-icon.png" alt="Momo logo" width={40} height={40} priority />
             <span className="ml-2 text-lg font-bold">Momo</span>
           </Link>
