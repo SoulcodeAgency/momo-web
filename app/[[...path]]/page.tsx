e85ee1aa69db44946ab4922249d6fa6ec3b1a4c6
Fabian Gander <fabian.gander@soulcode.ch>
2024-10-17 17:58:55 +0200

    feat(PostHog) Implemented server side

--- app/[[...path]]/page.tsx
+++ app/[[...path]]/page.tsx
@@ -1,11 +1,19 @@
 import { resolveComponent } from '@/integrations/uniform/UniformComponentResolver';
 import { PageParameters, retrieveRoute, UniformComposition } from '@uniformdev/canvas-next-rsc';
+import PostHogClient from "../../integrations/posthog/posthog";
+import Link from 'next/link';
 
 // Uncomment to statically render routes at build time
 // export { generateStaticParams } from '@uniformdev/canvas-next-rsc';
 
 export default async function Home(props: PageParameters) {
   const route = await retrieveRoute(props);
+  const posthog = PostHogClient()
+  const flags = await posthog.getAllFlags(
+    'user_distinct_id' // replace with a user's distinct ID
+  );
+  await posthog.shutdown();
+
   return (
     <>
       <UniformComposition
@@ -16,6 +24,10 @@ export default async function Home(props: PageParameters) {
         // for the static mode (SSG) use mode="static"
         mode="server"
       />
+      {/* Flag example */}
+      {flags['main-cta'] &&
+        <Link href="http://posthog.com/">Go to PostHog</Link>
+      }
     </>
   );
 }
