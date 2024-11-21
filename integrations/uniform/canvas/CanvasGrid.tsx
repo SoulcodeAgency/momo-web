d7afdfcaeca9805b411c55036610f0f94b751b61
Fabian Gander <fabian.gander@soulcode.ch>
2024-11-21 10:45:03 +0100

    feat(Grid) Extending grid functionality with alignment

--- integrations/uniform/canvas/CanvasGrid.tsx
+++ integrations/uniform/canvas/CanvasGrid.tsx
@@ -2,14 +2,21 @@ import { UniformSlot } from '@uniformdev/canvas-next-rsc/component';
 import { CanvasProps } from '../typeHelpers';
 import { cn } from '@/lib/tailwind/tailwindClassName';
 
-type Parameters = { mdColumns: number };
+type Parameters = {
+  mdColumns: number;
+  align?: 'start' | 'center' | 'end';
+  justify?: 'start' | 'center' | 'end' | 'between' | 'around';
+};
 type SlotNames = 'content';
 type Props = CanvasProps<Parameters, SlotNames>;
 
-export function CanvasGrid({ component, context, slots, mdColumns }: Props) {
+export function CanvasGrid({ component, context, slots, mdColumns, align, justify }: Props) {
   const columnClasses = `md:grid-cols-${mdColumns ?? 3}`;
+  const alignClasses = align ? `items-${align}` : '';
+  const justifyClasses = justify ? `justify-${justify}` : '';
+
   return (
-    <div className={cn('grid grid-cols-1 gap-8 w-full max-w-5xl', columnClasses)}>
+    <div className={cn('grid grid-cols-1 gap-8 w-full max-w-5xl', columnClasses, alignClasses, justifyClasses)}>
       <UniformSlot context={context} data={component} slot={slots.content} />
     </div>
   );
