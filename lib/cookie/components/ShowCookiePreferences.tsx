f553bcfb813e7f11faa9ab197db5793c0bc2ba1f
Fabian Gander <fabian.gander@soulcode.ch>
2024-12-19 10:34:34 +0100

    Fixed cookie button covering footer links

--- lib/cookie/components/ShowCookiePreferences.tsx
+++ lib/cookie/components/ShowCookiePreferences.tsx
@@ -7,7 +7,7 @@ const ShowCookiePreferences = () => {
   const t = useTranslations('ShowCookiePreferences');
 
   return (
-    <div className="md:absolute flex justify-center md:mt-[-50px] left-0 right-0">
+    <div className="md:absolute flex justify-center md:mt-[-50px] left-0 right-0 md:w-max md:left-1/2 md:transform md:-translate-x-1/2">
       <span
         className="group flex cursor-pointer items-center gap-x-3 rounded-md p-2 text-sm leading-6 text-gray-900 hover:bg-primary hover:text-white dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white"
         onClick={(e) => {
